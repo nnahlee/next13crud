b06942f991d17677214248b13bf967770fda1982
Han Lee <bearchanlee@gmail.com>
2023-08-07 13:01:44 -0700

    added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 // csr
 import Users from "./components/Users";
 
+export const dynamic = "force-dynamic";
 // ssr In Home page
 async function getUsers() {
   try {
-    const res = await fetch(`${process.env.BASE_URL}/api/users`);
+    const res = await fetch(`${process.env.BASE_URL}/api/users`, {
+      cache: "no-store",
+    });
 
     if (!res.ok) {
       throw new Error("fetch failed");
