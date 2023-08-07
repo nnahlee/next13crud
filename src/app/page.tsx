1fc38b8464175a4cefdfe9069bb8b26959a625f4
Han Lee <bearchanlee@gmail.com>
2023-08-07 11:41:55 -0700

    removed refetch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,11 @@
 // csr
 import Users from "./components/Users";
-import AddUser from "./components/AddUser";
-// import { prisma } from "../../db/connect";
+
 // ssr In Home page
 async function getUsers() {
   // revalidate crud state from server every 4s
-  const res = await fetch(`http://localhost:3000/api/users`, {
-    next: {
-      revalidate: 4,
-    },
-  });
+  const res = await fetch(`http://localhost:3000/api/users`);
 
-  if (!res.ok) {
-    throw new Error("fetch failed");
-  }
   return res.json();
 }
 
