39bf1c9b0fea679f24b35cedccf5e9b4ea8db319
Han Lee <bearchanlee@gmail.com>
2023-08-07 12:19:03 -0700

    added next config to fix routing

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const dns = require("dns");
+dns.setDefaultResultOrder("ipv4first");
 
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+module.exports = nextConfig;
